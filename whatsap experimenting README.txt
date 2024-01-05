c760476cb666f129e29012d70c93d9c75de855d6
Pranav@pythoncoder <120322567+pranavmanglani28b@users.noreply.github.com>
2024-01-05 21:57:44 +0530

    Add files via upload

---
+++ whatsap experimenting README.txt
@@ -0,0 +1,9 @@
+This is created by Pranav Manglani on 5th, January 2024
+
+This will automate new year wishing oon opening whatsapp and running this program and minimizing the code editor
+
+please install pyautogui library by - 
+
+1) Opening powershell
+2) Typing - pip install pyautogui
+3) Pressing enter
---
+++ whatsapp experimenting.py
@@ -0,0 +1,42 @@
+import pyautogui as pg
+import time
+
+#1
+
+time.sleep(5)
+pg.moveTo(x=188, y=197)
+pg.click()
+time.sleep(2)
+pg.write("Happy New Year")
+pg.press('Enter')
+time.sleep(2)
+
+#2
+
+#(x=197, y=246)
+pg.moveTo(x=197, y=246)
+pg.click() 
+time.sleep(2)
+pg.write("Happy New Year")
+pg.press('Enter')
+time.sleep(2)
+
+#3
+
+#(x=204, y=336)
+pg.moveTo(x=204, y=336)
+pg.click() 
+time.sleep(2)
+pg.write("Happy New Year")
+pg.press('Enter')
+time.sleep(2)
+
+#4
+
+#(x=201, y=404)
+pg.moveTo(x=201, y=404)
+pg.click() 
+time.sleep(2)
+pg.write("Happy New Year")
+pg.press('Enter')
+time.sleep(2)
